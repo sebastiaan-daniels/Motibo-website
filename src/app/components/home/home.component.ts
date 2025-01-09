9b56ec0af8990d730b92dcea15ea2387fff12ee9
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-01-09 17:49:06 +0100

    Home site basics

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+    router = new Router;
+    videoPlayed = false;
+    
+    navigateTo(path: string) {
+      this.router.navigate([path]);
+    }
 }
