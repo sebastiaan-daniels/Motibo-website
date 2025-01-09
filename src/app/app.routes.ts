5cd90b3ac08c1368484b960b0b941f45820f1439
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-01-09 17:54:03 +0100

    Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CfmotoComponent } from './components/cfmoto/cfmoto.component';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'cfmoto', component: CfmotoComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'about-us', component: AboutUsComponent },
 ];
