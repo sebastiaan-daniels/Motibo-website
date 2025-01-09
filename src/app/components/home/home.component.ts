e90faedb14bb3d82417eb0c44143f9ebb220fc4b
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-01-09 20:00:54 +0100

    Working carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,22 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { CarouselComponent } from '../../subcomponents/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  carouselImages: string[] = [
+    '/home-carousel/car1.jpg',
+    '/home-carousel/car2.jpg',
+    '/home-carousel/car3.jpg',
+    '/home-carousel/car4.jpg',
+    '/home-carousel/car5.jpg',
+  ];
     router = new Router;
     videoPlayed = false;
     
---
+++ src/app/subcomponents/carousel/carousel.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+
+export class CarouselComponent implements OnInit, OnDestroy {
+  @Input() images: string[] = []; // Input to receive images
+  currentSlide: number = 0;
+  autoSlideInterval!: ReturnType<typeof setInterval>; // Reference for the interval
+
+  ngOnInit() {
+    this.startAutoSlide(); // Start auto-sliding when the component initializes
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide(); // Stop auto-sliding when the component is destroyed
+  }
+
+  startAutoSlide() {
+    this.autoSlideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Slide every 5 seconds
+  }
+
+  stopAutoSlide() {
+    clearInterval(this.autoSlideInterval);
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentSlide =
+      (this.currentSlide - 1 + this.images.length) % this.images.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+    this.stopAutoSlide(); // Reset auto-slide when manually navigating
+    this.startAutoSlide();
+  }
+}
