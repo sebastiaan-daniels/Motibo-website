8ce584452a7663fa93694900e7222d0c5eb5ee3e
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-01-10 15:07:28 +0100

    CFMoto page

--- src/app/components/cfmoto/cfmoto.component.ts
+++ src/app/components/cfmoto/cfmoto.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from "../../subcomponents/carousel/carousel.component";
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cfmoto',
   standalone: true,
-  imports: [],
+  imports: [CarouselComponent, RouterModule],
   templateUrl: './cfmoto.component.html',
   styleUrl: './cfmoto.component.css'
 })
 export class CfmotoComponent {
-
+  carouselImages: string[] = [
+    '/cf-moto-carousel/car1.jpg',
+    '/cf-moto-carousel/car2.jpg',
+    '/cf-moto-carousel/car3.jpg',
+  ];
 }
