3ebc5fe74c44fde0ff097b56ebfff85b5bd94a7b
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-01-18 11:16:24 +0100

    Fix mailto

---
+++ src/app/subcomponents/bike-card/bike-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './bike-card.component.html',
+  styleUrl: './bike-card.component.css'
+})
+export class BikeCardComponent {
+
+}
