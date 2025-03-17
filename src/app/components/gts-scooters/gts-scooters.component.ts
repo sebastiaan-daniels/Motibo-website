b1627804c975a2269514f7793c2f52ec7d7a927d
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-03-17 21:38:47 +0100

    Add GTS Scooters

--- src/app/components/gts-scooters/gts-scooters.component.ts
+++ src/app/components/gts-scooters/gts-scooters.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from "../../subcomponents/carousel/carousel.component";
+import { Router, RouterModule } from '@angular/router';
+import { BikeCardComponent } from "../../subcomponents/bike-card/bike-card.component";
+import { BikeCardExternComponent } from '../../subcomponents/bike-card-extern/bike-card-extern.component';
+
 
 @Component({
   selector: 'app-gts-scooters',
   standalone: true,
-  imports: [],
+  imports: [CarouselComponent, RouterModule, BikeCardExternComponent],
   templateUrl: './gts-scooters.component.html',
   styleUrl: './gts-scooters.component.css'
 })
 export class GtsScootersComponent {
-
+  carouselImages: string[] = [
+    '/gts-carousel/car1.webp',
+    '/gts-carousel/car2.webp',
+    '/gts-carousel/car3.webp',
+  ];
 }
