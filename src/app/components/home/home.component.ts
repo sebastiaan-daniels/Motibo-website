f60ed5124efaaa33fc13edb9a68bdd7473149942
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-04-06 13:29:13 +0200

    Basic advert modal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,38 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CarouselComponent } from '../../subcomponents/carousel/carousel.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, CarouselComponent],
+  imports: [RouterModule, CarouselComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  ngOnInit(): void {
+    const dismissedTimestamp = localStorage.getItem('motibo_advert_dismissed_until');
+
+    if (!dismissedTimestamp) {
+      this.showModal = true;
+    } else {
+      const dismissedUntil = new Date(dismissedTimestamp);
+      const now = new Date();
+      this.showModal = now > dismissedUntil;
+    }
+  }
+
+  dismissModal(): void {
+    const now = new Date();
+    const dismissedUntil = new Date(now.getTime() + 24 * 60 * 60 * 1000); // 24 hours from now
+    localStorage.setItem('motibo_advert_dismissed_until', dismissedUntil.toISOString());
+    this.showModal = false;
+  }
+  
+  showModal = true;
+
   carouselImages: string[] = [
     '/home-carousel/car1.jpg',
     '/home-carousel/car2.jpg',
