c7d96b753c5de103355d6da4500a4cd94d99010e
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-08-19 22:09:44 +0200

    Update ad modal

---
+++ src/app/subcomponents/ad-modal/ad-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './ad-modal.component.html',
+  styleUrl: './ad-modal.component.css'
+})
+export class AdModalComponent {
+
+}
